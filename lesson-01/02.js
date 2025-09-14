e706205eeb279a95c6c03296f7390a8cb4a5abbf
Ilja-Prihach <piratehnik9508@gmail.com>
2025-09-14 11:03:29 +0300

    wre

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -5,5 +5,23 @@
   Например, факториал числа 3 это 1 * 2 * 3 (6).
 */
 
+OLD_EMAIL="piratehnik9508@gmail.com"
+CORRECT_NAME="Ilja-Prihach"
+CORRECT_EMAIL="piratehnik9508@gmail.com"
+
+git filter-branch --env-filter '
+if [ "$GIT_COMMITTER_EMAIL" = "'"$piratehnik9508@gmail.com"'" ]
+then
+export GIT_COMMITTER_NAME="'"$Ilja-Prihach"'"
+export GIT_COMMITTER_EMAIL="'"$piratehnik9508@gmail.com"'"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "'"$piratehnik9508@gmail.com"'" ]
+    then
+export GIT_AUTHOR_NAME="'"$Ilja-Prihach"'"
+export GIT_AUTHOR_EMAIL="'"$piratehnik9508@gmail.com"'"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+
 // your code
 
